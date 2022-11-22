7370107943ec2c8d853b095f90f56cec4c838795
Derik Evangelista <derik@syntasso.io>
2022-11-22 14:49:16 +0000

    kcduk: remove extra jenkins pipeline

--- scripts/quick-start.sh
+++ scripts/quick-start.sh
@@ -196,13 +196,11 @@ install_kratix() {
     docker pull syntasso/kratix-platform-work-creator:a8ecc5698298a1ff4fa777822df30d3b8fab326d
     docker pull bitnami/kubectl:1.20.10
     docker pull syntasso/kratix-platform:a8ecc5698298a1ff4fa777822df30d3b8fab326d
-    docker pull virtuslab/jenkins-operator:v0.7.1
     docker pull minio/minio:RELEASE.2022-06-30T20-58-09Z
 
     kind load docker-image syntasso/kratix-platform:a8ecc5698298a1ff4fa777822df30d3b8fab326d --name platform
     kind load docker-image syntasso/kratix-platform-work-creator:a8ecc5698298a1ff4fa777822df30d3b8fab326d --name platform
     kind load docker-image bitnami/kubectl:1.20.10 --name platform
-    kind load docker-image syntasso/jenkins-request-pipeline --name platform
     kind load docker-image minio/minio:RELEASE.2022-06-30T20-58-09Z --name platform
 
     if ${LOCAL_IMAGES}; then
@@ -223,6 +221,7 @@ install_kratix() {
         exit 1
     fi
 
+    docker pull virtuslab/jenkins-operator:v0.7.1
     docker pull jenkins/jenkins:2.332.2-jdk17
     kind load docker-image virtuslab/jenkins-operator:v0.7.1 --name worker
     kind load docker-image jenkins/jenkins:2.332.2-jdk17 --name worker
